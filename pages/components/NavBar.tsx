7ef816e2c31c02387c3df4b62b40e7dceccbde47
BoraParkDev <borapark13@gmail.com>
2023-05-05 18:57:20 +0900

    #modify styled-jsx apply

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -1,47 +1,20 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
-import styles from ".//NavBar.module.css";
-import { useEffect } from "react";
-import { useState } from "react";
 
 export default function NavBar() {
-  const [color, setColor] = useState("black");
-  const [color2, setColor2] = useState("black");
   const router = useRouter();
-
-  useEffect(() => {
-    if (router.pathname === "/") {
-      setColor("blue");
-      console.log("1");
-    }
-  }, [router.pathname]);
-
-  useEffect(() => {
-    if (router.pathname === "/about") {
-      setColor2("blue");
-      console.log("2");
-    }
-  }, [router.pathname]);
-
   return (
     <nav>
-      <Link
-        href="/"
-        className={`${styles.link} ${
-          router.pathname === "/" ? styles.active : ""
-        }`}
-      >
-        Home
-      </Link>
-      <Link
-        href="/about"
-        className={[
-          styles.link,
-          router.pathname === "/about" ? styles.active : "",
-        ].join(" ")}
-      >
-        About
-      </Link>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <style jsx>{`
+        nav {
+          background-color: #333;
+        }
+        a {
+          text-decoratin: none;
+        }
+      `}</style>
     </nav>
   );
 }
