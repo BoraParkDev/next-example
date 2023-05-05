e09e7233b6c0be14d8d4efa78fcfe2ec4669592a
BoraParkDev <borapark13@gmail.com>
2023-05-05 14:57:45 +0900

    #feat navigation conditional style add.

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -1,21 +1,34 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import styles from ".//NavBar.module.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function NavBar() {
+  const [color, setColor] = useState("black");
+  const [color2, setColor2] = useState("black");
   const router = useRouter();
-  console.log(router);
+
+  useEffect(() => {
+    if (router.pathname === "/") {
+      setColor("blue");
+      console.log("1");
+    }
+  }, [router.pathname]);
+
+  useEffect(() => {
+    if (router.pathname === "/about") {
+      setColor2("blue");
+      console.log("2");
+    }
+  }, [router.pathname]);
+
   return (
-    <nav>
-      <Link
-        style={{ color: router.pathname === "/" ? "red" : "blue" }}
-        href="/"
-      >
+    <nav className={styles.nav}>
+      <Link href="/" style={{ color: color }}>
         Home
       </Link>
-      <Link
-        style={{ color: router.pathname === "/about" ? "blue" : "red" }}
-        href="/about"
-      >
+      <Link href="/about" style={{ color: color2 }}>
         About
       </Link>
     </nav>
