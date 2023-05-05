56ed369df9e0e9f611db0d5dcb042c9275ee3da3
BoraParkDev <borapark13@gmail.com>
2023-05-06 01:02:36 +0900

    #feat global styles added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
-import NavBar from "./components/NavBar";
 import Layout from "./components/Layout";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
