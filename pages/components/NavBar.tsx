c07016e3e0795e560fc6332796c1846c8a6d6b3e
BoraParkDev <borapark13@gmail.com>
2023-05-06 23:35:56 +0900

    #moidfy navigation bar component updated.

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -1,21 +1,47 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link href="/">
-        <span className={router.pathname === "/" ? "active" : ""}>Home</span>
-      </Link>
-      <Link href="/about">
-        <span className={router.pathname === "/about" ? "active" : ""}>
-          About
-        </span>
-      </Link>
+      <img src="/vercel.svg" alt="logo" />
+      <div>
+        <Link href="/">
+          <span className={router.pathname === "/" ? "active" : ""}>Home</span>
+        </Link>
+        <Link href="/detail">
+          <span className={router.pathname === "/detail" ? "active" : ""}>
+            Detail
+          </span>
+        </Link>
+      </div>
       <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
         .active {
-          color: blue;
+          color: navy;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,53 @@
-import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Head from "next/head";
+import { useEffect, useState } from "react";
 import Seo from "./components/Seo";
 
+export type MovieObjectType = { [property: string]: string };
 export default function Home() {
+  const [movies, setMovies] = useState<MovieObjectType[]>([]);
+  useEffect(() => {
+    //effect는 컴포넌트가 렌더링되고 난 이후에 실행됨
+    //이 함수는 비동기로 동작. 렌더링 이후에 발생하는 모든 변경사항을 처리함
+    //이 함수를 즉시 실행하여 초기화 작업을 수행해야 할 때가 있음
+    //예를 들어 컴포넌트가 마운트될 때 함수를 즉시 실행하여 데이터 세팅 작업을 수행함
+    (async () => {
+      const resp = await fetch("/api/movies");
+      const { results } = await resp.json();
+      setMovies(results);
+    })();
+  }, []);
+  console.log(movies);
+
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
-      <h1>hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id} className="movie">
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
